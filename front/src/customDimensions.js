6054100b6568d336dffbcb146cdbdc581ab0d6a9
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2022-11-25 01:33:25 +0900

    modify TextBtn

--- front/src/customDimensions.js
+++ front/src/customDimensions.js
@@ -1,10 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import {
   responsiveScreenHeight,
   responsiveScreenWidth,
 } from 'react-native-responsive-dimensions';
 import {useWindowDimensions} from 'react-native';
 
-const {height, width} = useWindowDimensions();
 
 const FIGMA_WINDOW_WIDTH = 428;
 const FIGMA_WINDOW_HEIGHT = 926;
@@ -21,14 +21,14 @@ export function heightPercentage(size) {
   return responsiveScreenHeight(percentage);
 }
 
-export function fontPercentage(size) {
-  // const percentage = size*0.135;
-  //이건 알맞은 fonsize 비율을 찾아야 함.
+// export function fontPercentage(size) {
+//   // const percentage = size*0.135;
+//   //이건 알맞은 fonsize 비율을 찾아야 함.
 
-  // return responsiveScreenFontSize(percentage);
-  return (size / FIGMA_WINDOW_WIDTH) * width;
-}
+//   // return responsiveScreenFontSize(percentage);
+//   return (size / FIGMA_WINDOW_WIDTH) * useWindowDimensions().width;
+// }
 
-export function autoSetBorderRadius(size) {
-  return (size / FIGMA_WINDOW_WIDTH) * width;
-}
+// export function autoSetBorderRadius(size) {
+//   return (size / FIGMA_WINDOW_WIDTH) * useWindowDimensions().width;
+// }
