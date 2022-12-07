332ea10f524def0babba1d4ce00b8e90fdd77ca5
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2022-12-07 13:34:06 +0900

    building bottombar

--- front/App.js
+++ front/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import Tabs from './src/components/BottomBar';
 import BackBtn from './src/components/commonsComponents/BackBtn';
 import GrayShadowBox from './src/components/commonsStyles/GrayShadowBox';
+import { autoSetBorderRadius } from './src/customDimensions';
+import HomePage from './src/screens/HomePage';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View style={[GrayShadowBox.grayshadowbox, {margin: 15, width: '100%'}]}>
+      <View style={[GrayShadowBox.grayshadowbox, {margin: 15, width: '100%', borderRadius:autoSetBorderRadius(15)}]}>
         <Text style={{margin: 20}}>HI</Text>
         <BackBtn
           style={{margin:10, width:15, height:15}}
         />
       </View>
+      <Tabs/>
     </SafeAreaView>
   );
 };
---
+++ front/src/screens/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+import GrayShadowBox from "../components/commonsStyles/GrayShadowBox";
+import { autoSetBorderRadius } from "../customDimensions";
+import BackBtn from "../components/commonsComponents/BackBtn";
+
+const HomePage = () => {
+    return(
+        <SafeAreaView>
+            <View></View>
+        </SafeAreaView>
+    );
+}
+
+export default HomePage;
