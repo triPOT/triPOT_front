4e4920ab2626c30b239ba670f27c8dd8fa63703a
Chaemin-L <chaem12@kookmin.ac.kr>
2022-12-07 16:16:04 +0900

    chore: Add test board data

---
+++ front/src/components/board/testData.js
@@ -0,0 +1,201 @@
+// 임시 게시글 목록
+export const testPosts = [
+  {
+    id: 1, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-11-02',
+      location: 'seoul',
+    },
+    user: {
+      id: 123,
+      name: '차은우',
+      rank: '숙련된 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2016/02/11/16/59/dog-1194083_960_720.jpg',
+    },
+    content: {
+      image: 'https://cdn.pixabay.com/photo/2016/02/11/16/59/dog-1194083_960_720.jpg',
+      post: '오늘 날씨가 참 좋아요. 저랑 같이 제주도 가실 분?',
+    },
+  },
+  {
+    id: 2, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-03',
+      location: 'daejeon',
+    },
+    user: {
+      id: 123,
+      name: '서강준',
+      rank: '익숙한 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2020/08/21/08/46/african-5505598_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '내일 저녁 같이 안산에서 저녁 드실 분 구합니다~',
+    },
+  },
+  {
+    id: 3, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 126,
+      name: '안유진',
+      rank: '숙련된 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2015/03/03/08/55/portrait-657116_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '오늘 저녁 메뉴 추천 받아요',
+    },
+  },
+  {
+    id: 4, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 131,
+      name: '이도현',
+      rank: '초행길 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '안녕하세요',
+    },
+  },
+  {
+    id: 5, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 126,
+      name: '남준',
+      rank: '익숙한 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '저랑 같이 전국 투어 하실분~',
+    },
+  },
+  {
+    id: 10, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 140,
+      name: '제이슨',
+      rank: '초행길 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2022/10/19/01/02/woman-7531315_960_720.png',
+    },
+    content: {
+      image: '',
+      post: '이번 주말에 부산 여행 가실 분 계신가요?',
+    },
+  },
+  {
+    id: 12, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 144,
+      name: '세민',
+      rank: '초행길 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg',
+    },
+    content: {
+      image: 'https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg',
+      post: '을왕리 조개구이가 먹고 싶은 날이네요',
+    },
+  },
+  {
+    id: 44, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 131,
+      name: '이도현',
+      rank: '숙련된 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2016/11/29/06/08/woman-1867715_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '안녕하세요',
+    },
+  },
+  {
+    id: 51, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 126,
+      name: '남준',
+      rank: '초행길 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg',
+    },
+    content: {
+      image: '',
+      post: '저랑 같이 전국 투어 하실분~',
+    },
+  },
+  {
+    id: 19, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 140,
+      name: '제이슨',
+      rank: '숙련된 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2022/10/19/01/02/woman-7531315_960_720.png',
+    },
+    content: {
+      image: '',
+      post: '서울 맛집 탐방할 세 분 모셔요',
+    },
+  },
+  {
+    id: 23, // 게시글 아이디
+    meta: {
+      type: 'recruit',
+      date: '2022-12-04',
+      location: 'jejudo',
+    },
+    user: {
+      id: 144,
+      name: '세민',
+      rank: '초행길 여행자',
+      profileImage: 'https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg',
+    },
+    content: {
+      image: 'https://cdn.pixabay.com/photo/2020/08/21/08/46/african-5505598_960_720.jpg',
+      post: '을왕리 조개구이가 먹고 싶은 날이네요',
+    },
+  },
+];
